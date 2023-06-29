1bf4ad3f30bc72a3fcb1a8ec42d64b4dfcea5c9d
John <jonathan.ortega.bravo@hotmail.com>
2023-06-29 13:47:36 +0100

    Primer commit inicio psvm

---
+++ Dinosaurio/src/main/java/propio/dinosaurio/Principal.java
@@ -0,0 +1,16 @@
+
+package propio.dinosaurio;
+
+/**
+ *
+ * @author ortbra
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("pruebsa");
+        
+    }
+    
+}
